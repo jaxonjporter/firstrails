a56eb98a3d2c65212d558f920d203c7f4a8adb21
Jaxon Porter <jaxonutah@gmail.com>
2019-02-18 16:42:38 -0700

    Finished basic objectives

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,4 +10,35 @@ class PagesController < ApplicationController
   def new
     @pages = Page.new
   end
+  def create 
+    @pages = Page.new(pages_params)
+    if @pages.save
+      redirect_to pages_path
+    else
+      render :new
+    end
+  end
+
+  def edit 
+    @pages = Page.find(params[:id])
+  end
+
+  def update
+    @page = Page.find(params[:id])
+    if @page.update(pages_params)
+      redirect_to pages_path
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @pages = Page.find(params[:id]).destroy
+    redirect_to pages_path
+  end
+  private
+    def pages_params
+      params.require(:page).permit(:title, :author, :body)
+    end
+
+
 end
